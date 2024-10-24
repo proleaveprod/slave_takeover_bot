5bd44a6b6ee657196bb4ff70ceec8c4e55187a01
Червинко Евгений <chervinko.ei@starline.ru>
2024-10-24 14:58:22 +0300

    [MOD] File logging

--- slave_takeover_bot.py
+++ slave_takeover_bot.py
@@ -1,6 +1,10 @@
 import os, threading, json
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+from file_logger import logger
+print("Main: Program is started")
+logger.info('Main: Program is started')
+
 from bot import bot_start
 from google_sheets import carTable
 
@@ -8,11 +12,6 @@ bot_configs = dict()
 with open('settings/settings.json', 'r', encoding='utf-8') as file:
     bot_configs = json.load(file)
 
-def telebot_handle():
-    bot_start()
-
 if __name__ == '__main__':
-
     carTable.update()
-    telebot_thread = threading.Thread(target=telebot_handle)
-    telebot_thread.start()
+    bot_start()
